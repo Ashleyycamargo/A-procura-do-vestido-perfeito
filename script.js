94a20622d0f98adba80cd302cab4cbdbcee23c66
Ashley Camargo <00001072579765SP@al.educacao.sp.gov.br>
2024-09-03 11:20:08 -0300

    Add files via upload
    Figma: https://www.figma.com/board/CO19EtO60SMgTlLbGMj31t/Untitled?node-id=1-12&node-type=SHAPE_WITH_TEXT&t=wp7Nttjx28f8Ld1N-0

---
+++ script.js
@@ -0,0 +1,12 @@
+const avanca = document.querySelectorAll('.btn-próximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        console.log(proximoPasso);
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
